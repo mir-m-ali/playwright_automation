8df2c73b130e9b1bad03e0176fcdc6cf6f70ad5a
Mir Ali <mir.ali1016@gmail.com>
2026-01-06 11:07:40 -0500

    PA-101: fixed bugs

--- github-action.js
+++ github-action.js
@@ -1,1 +1,16 @@
 console.log("Running GitHub Action script...");
+let commitMessage = process.argv[2];
+if (commitMessage !== undefined) {
+  console.log(commitMessage);
+  let index = commitMessage.indexOf(":");
+  if (index !== -1) {
+    let jiraId = commitMessage.substring(0, index);
+    let message = commitMessage.substring(index + 1).trim();
+    console.log(`Jira ID: ${jiraId}`);
+    console.log(`message: ${message}`);
+  } else {
+    console.log("Commit message not in expected format!");
+  }
+} else {
+  console.log("No commit message provided.");
+}
