fed6954f27ad55faed8913e3434039f3eeb9b23a
Mir Ali <mir.ali1016@gmail.com>
2026-01-07 12:48:29 -0500

    testing

---
+++ test-github-action.js
@@ -0,0 +1,57 @@
+const EMAIL = `mir.ali1016@gmail.com`;
+const API_TOKEN =
+  "ATATT3xFfGF0GaESkO-Yy7mEar2Mrz-DJWEdvQBLzraTl7kMqV7zyPzrKCK3NwX46jHQUd_pgC9qv27U2LcAhkwo-nKMKWni0GowQALbPz6LGCIV3nPSIs7t4lJcQepCOgqCsZ_-U1jS6k3xLjwVfIPs1G55dHQCncrotINDSE85WE8QLBGhqB8=F3EDEF91";
+
+const AUTH = Buffer.from(`${EMAIL}:${API_TOKEN}`).toString("base64");
+const DOMAIN = `mirmali.atlassian.net`;
+
+async function updateJiraField(jiraId, message) {
+  const bodyData = {
+    fields: { customfield_10088: new Date().toISOString() + ": " + message },
+  };
+  try {
+    const response = await fetch(
+      `https://${DOMAIN}/rest/api/3/issue/${jiraId}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Basic ${AUTH}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+      }
+    );
+
+    if (response.status === 204) console.log("Jira issue updated successfully");
+    else {
+      const error = await response.text();
+      console.error(`Error ${response.status}: ${error}`);
+    }
+  } catch (err) {
+    console.log("Fetch error:", err);
+  }
+}
+
+async function getJiraTicketDetails(jiraId) {
+  const response = await fetch(
+    `https://${DOMAIN}/rest/api/3/issue/${jiraId}?fields=summary,status,assignee`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${AUTH}`,
+        Accept: "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    console.error(`Error---> ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+}
+
+getJiraTicketDetails("PAP-3");
+//runGitHubAction();
