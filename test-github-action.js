aea6f62ba355d0528f9cc391fd5256bbc95262fe
Mir Ali <mir.ali1016@gmail.com>
2026-01-08 09:06:47 -0500

    printing length of token

--- test-github-action.js
+++ test-github-action.js
@@ -1,5 +1,6 @@
 const EMAIL = `mir.ali1016@gmail.com`;
 const API_TOKEN = process.env.JIRA_API_TOKEN;
+console.log(`length of API token: ${API_TOKEN.length}`);
 const AUTH = Buffer.from(`${EMAIL}:${API_TOKEN}`).toString("base64");
 const DOMAIN = `mirmali.atlassian.net`;
 
